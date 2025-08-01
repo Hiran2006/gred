0a59c73f9337227fb4fd0f0e1df973f3422875e4
Hiran <hirans2006@gmail.com>
2025-08-01 10:15:08 +0530

    build 3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./Home.tsx";
-import NotFound from "./NotFound.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index path="" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
+        <Route index path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
