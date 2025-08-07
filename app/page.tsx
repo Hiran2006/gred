93e52e044458764d6359233ed0b2b5aa36f7b87b
Hiran <hirans2006@gmail.com>
2025-08-07 22:18:56 +0530

    sapce align of search and location

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <div className="flex justify-center items-center mt-8 mb-10">
         <Image src={logo} alt="logo" width={180} />
       </div>
-      <div className="flex justify-between items-center align-center w-full max-w-6xl px-4 mb-8">
+      <div className="flex justify-evenly items-center align-center w-full mb-8">
         <div className="flex justify-between items-center rounded-3xl bg-amber-100 p-1 h-9">
           <input
             type="text"
@@ -26,7 +26,7 @@ export default function Home() {
           <MagnifyingGlassIcon className="w-6 text-gray-500 mr-1" />
         </div>
         <div>
-          <div className="w-30 bg-gray-200 h-9 rounded-3xl flex items-center justify-center text-gray-500">
+          <div className="bg-gray-200 h-9 rounded-3xl flex items-center justify-center text-gray-500 p-3">
             <p>Location</p>
             <MapPinIcon className="w-5 h-5 ml-2 text-gray-500" />
           </div>
