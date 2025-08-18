197a6df817390d65d4a44a2253780708c2e41f41
Hiran S <hirans2006@gmail.com>
2025-08-18 19:55:36 +0530

    navigation fix

--- components/GoogleAuthButton.tsx
+++ components/GoogleAuthButton.tsx
@@ -4,7 +4,6 @@ import { FcGoogle } from "react-icons/fc";
 import { Provider } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import styles from "@/app/login/login.module.css";
 import supabase from "@/lib/supabase";
 
 type GoogleAuthButtonProps = {
@@ -51,13 +50,17 @@ export default function GoogleAuthButton({
 
   return (
     <button
-      className={`${styles.button} ${styles.googleButton}`}
+      className="w-full max-w-[400px] h-[50px] rounded-[10px] bg-white text-gray-800 font-medium flex items-center justify-center gap-3 border-2 border-gray-200 hover:border-amber-400 focus:outline-none focus:border-gray-800 transition-[150ms] ease-in-out disabled:opacity-50 disabled:cursor-not-allowed"
       type="button"
       onClick={() => handleOAuth("google")}
       disabled={isLoading || isButtonLoading}
     >
-      <FcGoogle className={styles.googleIcon} />
-      {isLoading || isButtonLoading ? "Please wait..." : "Continue with Google"}
+      <FcGoogle className="text-2xl" />
+      <span>
+        {isLoading || isButtonLoading
+          ? "Please wait..."
+          : "Continue with Google"}
+      </span>
     </button>
   );
 }
